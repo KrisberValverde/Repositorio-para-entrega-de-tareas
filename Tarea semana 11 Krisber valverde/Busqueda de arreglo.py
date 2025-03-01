438feb1d2c193d549ac2d3505e005d00b5936285
Krisber Valverde <crisbervalverde07@gmail.com>
2025-03-01 16:58:41 -0500

    Programación en Python y sincronización en GitHub

---
+++ Tarea semana 11 Krisber valverde/Busqueda de arreglo.py
@@ -0,0 +1,14 @@
+#Matriz de bidimensional 3x3
+
+Numeros_pares= [[2,4,6],
+                [8,10,12],
+                [14,16,18]
+                       ]
+
+valor_a_encontrar = 10
+for fila in range (len(Numeros_pares)):
+    for columna in range (len(Numeros_pares[fila])):
+        if Numeros_pares[fila][columna] == valor_a_encontrar:
+            print(f"Ubicado en la fila {fila} "
+                  f"y la columna {columna} "
+                  f"el valor a encontrar es: {Numeros_pares[fila][columna]}")
